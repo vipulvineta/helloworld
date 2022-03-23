4e546681a285f953f110ebc377798da268891f57
vipulvineta <43403307+vipulvineta@users.noreply.github.com>
2022-03-23 15:45:34 +0530

    Create startcontainer.ps1

---
+++ startcontainer.ps1
@@ -0,0 +1,2 @@
+docker build -t dockeragent:latest .
+docker run --once -e AZP_URL=https://dev.azure.com/vipulvineta/ -e AZP_TOKEN=4owyeg5mjnxos2eckzi3a6ifwthuwl3eto7f5bpjnm5drtf64gva -e AZP_AGENT_NAME=mydockeragent dockeragent:latest
