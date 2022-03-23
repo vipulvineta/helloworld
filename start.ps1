6e396b5b5a35e29acd65438dc1ef1dd07e200247
vipulvineta <43403307+vipulvineta@users.noreply.github.com>
2022-03-23 11:38:36 +0530

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,2 +1,2 @@
 docker pull mcr.microsoft.com/windows/nanoserver:1803-amd64
-docker run --rm mcr.microsoft.com/windows/nanoserver:1803-amd64
+docker run mcr.microsoft.com/windows/nanoserver:1803-amd64
